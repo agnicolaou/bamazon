b832c448f454d9d040d9888019edc0cf449a561e
Andreas Nicolaou <49968813+agnicolaou@users.noreply.github.com>
2019-08-26 20:01:39 -0400

    Added import data, prompt user, ask for quantity functions

---
+++ bamazonCustomer.js
@@ -0,0 +1,70 @@
+var inquirer = require("inquirer");
+var mySQL = require("mysql");
+require("console.table");
+
+var connection = mySQL.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazonDB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    importData();
+})
+
+function importData() {
+    connection.query("SELECT * FROM products", function (err, response) {
+        console.table(response);
+        prompt();
+    });
+}
+
+function prompt() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "userID",
+                message: "Choose a product ID."
+            }
+        ]).then(function (response) {
+            var choiceID = parseInt(response.userID)
+            var product = checkInventory(choiceID, inventory)
+
+            if (product) {
+                askForQuantity(product)
+            }
+            else {
+                console.log("That item is not in the inventory")
+                importData()
+            }
+        })
+};
+
+// askForQuantity
+function askForQuantity(userChoice) {
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "qty",
+            message: "How many would you like to order?"
+        }
+    ]).then(function(response){
+        var quantity = parseInt(response.qty);
+        if (quantity > userChoice.stock_qty){
+            console.log("Sorry, not enough in stock.");
+            importData();
+        } else {
+            makePurchase(product, quantity)
+        }
+    })
+};
+
+
+// makepurchase
+
+// checkInventory
